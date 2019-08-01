9b9648c99b23e18e39d007fedc69186a69d79e2b
Duc Vu <de.yd315@gmail.com>
2019-08-01 07:56:07 -0500

    give write method ability to modify pointDurabilit

--- pencil.js
+++ pencil.js
@@ -3,8 +3,25 @@ class Pencil {
     this.pointDuribility = property.pointDuribility;
   }
 
-  write(text, paper) {
-    paper.content += text;
+  write(text, paper = { content: "" }) {
+    let newText = "";
+    for (let i = 0; i < text.length; i++) {
+      if (this.pointDuribility) {
+        const char = text[i];
+        newText += char;
+        if (char !== " ") {
+          if (char.toLowerCase() === char) {
+            this.pointDuribility--;
+          } else {
+            this.pointDuribility -= 2;
+          }
+        }
+      } else {
+        newText += " ";
+      }
+    }
+    paper.content += newText;
+    return paper;
   }
 }
 
--- pencil.spec.js
+++ pencil.spec.js
@@ -5,32 +5,57 @@ const properties = {
   pointDuribility: 40000
 };
 
+const draft = new Paper();
 const note = new Paper();
 const shoppingList = new Paper("apples");
-const pencil = new Pencil(properties);
+const dullPencil = new Pencil();
 
 describe("Pencil Class", () => {
   describe("Constructor method", () => {
     test("it constructs a new pencil object with specified properties", () => {
+      const pencil = new Pencil(properties);
       expect(pencil.pointDuribility).toBe(40000);
     });
 
     test("it constructs a new pencil object with default properties", () => {
-      const defaultPencil = new Pencil();
-      expect(defaultPencil.pointDuribility).toBe(0);
+      expect(dullPencil.pointDuribility).toBe(0);
     });
   });
 
   describe("Write method", () => {
-    test("it appends text to the content of the paper", () => {
-      const reminder = "finish the kata as soon as possible";
-      const items = " ,oranges, milk, cereal, bread, eggs";
+    describe("pointy case", () => {
+      const pencil = new Pencil({ pointDuribility: 100 });
+      const reminder = "finish the kata as soon as possible"; //29
+      const items = " ,oranges, milk, cereal, bread, eggs"; //31
       pencil.write(reminder, note);
-      pencil.write(" ,oranges, milk, cereal, bread, eggs", shoppingList);
-      expect(note.content).toBe(reminder);
-      expect(shoppingList.content).toBe(
-        "apples ,oranges, milk, cereal, bread, eggs"
-      );
+      console.log(pencil.pointDuribility);
+      pencil.write(items, shoppingList);
+      console.log(pencil.pointDuribility);
+
+      test("it appends text to the content of the paper", () => {
+        expect(note.content).toBe(reminder);
+        expect(shoppingList.content).toBe(
+          "apples ,oranges, milk, cereal, bread, eggs"
+        );
+      });
+
+      test("it degrades the pointiness of pencil", () => {
+        const reminderLength = reminder.split("").filter(c => c !== " ").length;
+        const itemsLength = items.split("").filter(c => c !== " ").length;
+        expect(pencil.pointDuribility).toBe(100 - reminderLength - itemsLength);
+      });
+    });
+
+    describe("dull case", () => {
+      test("it appends a white space to the content of the paper", () => {
+        dullPencil.write("I am invisible", draft);
+        expect(draft.content).toBe("              ");
+        dullPencil.write(" ,butter knife", shoppingList);
+        expect(shoppingList.content).toBe(
+          "apples ,oranges, milk, cereal, bread, eggs              "
+        );
+        expect(dullPencil.pointDuribility).toBe(0);
+      });
     });
   });
 });
