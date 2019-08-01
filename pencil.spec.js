7e8723f8de261f60b9c774ad6c420ddf4582c633
Duc Vu <de.yd315@gmail.com>
2019-07-31 23:49:17 -0500

    refactored pencil spec file

--- pencil.spec.js
+++ pencil.spec.js
@@ -1,11 +1,13 @@
-const Pencil = require('./pencil')
+const Pencil = require("./pencil");
 
-describe('Pencil Class', () => {
-    test('Constructor method constructs a new pencil object with some properties', () => {
-        const properties = {
-            pointDuribility: 40000,
-        }
-        const pencil = new Pencil(properties)
-        expect(pencil.pointDuribility).toBe(40000)
-    })
-})
+describe("Pencil Class", () => {
+  describe("Constructor method", () => {
+    test("it constructs a new pencil object with some properties", () => {
+      const properties = {
+        pointDuribility: 40000
+      };
+      const pencil = new Pencil(properties);
+      expect(pencil.pointDuribility).toBe(40000);
+    });
+  });
+});
