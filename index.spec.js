6cbb2cde2844751a5b98ed33b5d68aac775a801d
Duc Vu <de.yd315@gmail.com>
2019-08-09 03:53:33 -0500

    write() throw err when point property of a pencil object is inapporiate

--- index.spec.js
+++ index.spec.js
@@ -5,8 +5,22 @@ const paper = {};
 const text = "Let's start writing";
 
 describe("write", () => {
-  describe("instructs pencil to write on a sheet of paper", () => {
-    test("", () => {
+  describe("instructs a pencil to write on a sheet of paper", () => {
+    test("throws an err if pencil point is undefined", () => {
+      expect(() => {
+        write(pencil, paper, text);
+      }).toThrow("Pencil Is Dull");
+    });
+
+    test("throws an err if pencil point is null", () => {
+      pencil.point = null;
+      expect(() => {
+        write(pencil, paper, text);
+      }).toThrow("Pencil Is Dull");
+    });
+
+    test("throws an err if pencil point is undefined", () => {
+      pencil.point = 0;
       expect(() => {
         write(pencil, paper, text);
       }).toThrow("Pencil Is Dull");
