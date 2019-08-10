96177fa88e1276221797e019ecae1727fbf2869b
Duc Vu <de.yd315@gmail.com>
2019-08-09 20:23:25 -0500

    err msg updated

--- errors.js
+++ errors.js
@@ -1,12 +1,13 @@
 const PencilPointError = {
   name: "PencilPointError",
   message:
-    "the point value of this pencil must be a positive integer and less than or equal to the fullPoint value"
+    "the point value of this pencil must be zero or a positive integer and cannot be greater than fullPoint value"
 };
 
 const PencilFullPointError = {
   name: "PencilFullPointError",
-  message: "the fullPoint value of this pencil must be a positive integer"
+  message:
+    "the fullPoint value of this pencil must be a positive integer and cannot be less than the point value"
 };
 
 module.exports = {
